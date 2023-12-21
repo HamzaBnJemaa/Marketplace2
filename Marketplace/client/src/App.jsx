89e3fcb707e8cace6979a4f9b8b585b7ef8acf22
azizchannoufi <channoufi2003@gmail.com>
2023-12-21 19:05:27 +0100

    newdone

--- Marketplace/client/src/App.jsx
+++ Marketplace/client/src/App.jsx
@@ -5,17 +5,24 @@ import Categories from "./pages/categories/Categories.jsx";
 import Products from "./pages/products/Products.jsx";
 import Create from "./pages/Create/Create.jsx";
 import Onepage from "./pages/Onepage/Onepage.jsx";
+import Official from "./Signup&Login/official";
+import axios from "axios"
+import Navbar from "./page_comp/navbar";
 
 
 
 
 function App() {
 const [idCategorie,setCategorie]=useState(0)
+axios.defaults.withCredentials=true
+
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar/>
         <Routes>
-          <Route path="/" element={<Home/>}></Route>
+          <Route path="/" element={<Official/>}></Route>
+          <Route path="/home" element={<Home/>}></Route>
           <Route path="/categories" element={<Categories setCategorie={setCategorie} />} ></Route>
           <Route path="/products" element={<Products idCategorie={idCategorie} />}></Route> 
           <Route path="/create" element={<Create />}></Route>
